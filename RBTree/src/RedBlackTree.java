87fe7900a0ab35147a5078e891188b8c1977ad10
Yuan Gao <gaoyuanbj@yahoo.com>
2018-03-15 11:40:00 -0400

    Unfinished insert

--- RBTree/src/RedBlackTree.java
+++ RBTree/src/RedBlackTree.java
@@ -1,3 +1,5 @@
+import java.util.Optional;
+
 public class RedBlackTree {
     public enum Color {
         RED,
@@ -45,7 +47,107 @@ public class RedBlackTree {
     }
 
     public RBNode insert(RBNode parent, RBNode root, int val) {
-        return new RBNode();
+        if (root == null || root.isNullLeaf) {
+            if (parent != null) {
+                createRedNode(parent, val);
+            }
+            else {
+                createBlackNode(val);
+            }
+        }
+
+        if (root.val == val) {
+            throw new IllegalArgumentException("Duplicate val " + val);
+        }
+        boolean isLeft;
+        if (val < root.val) {
+            RBNode left = insert(root, root.left, val);
+            if (left == root.parent) {
+                return left;
+            }
+            root.left = left;
+            isLeft = true;
+        }
+        else {
+            RBNode right = insert(root, root.right, val);
+            if (right == root.parent) {
+                return right;
+            }
+            root.right = right;
+            isLeft = false;
+        }
+
+        if (isLeft) {
+            
+        }
+    }
+
+    private Optional<RBNode> findSiblingNode(RBNode root) {
+        RBNode parent = root.parent;
+        if (isLeftChild(root)) {
+            return Optional.ofNullable(parent.right.isNullLeaf ? null : parent.right);
+        }
+        else {
+            return Optional.ofNullable(parent.left.isNullLeaf ? null : parent.left);
+        }
+    }
+
+    private boolean isLeftChild(RBNode root) {
+        RBNode parent = root.parent;
+        if (parent.left == root) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    private void rightRotate(RBNode root, boolean changeColor) {
+        RBNode parent = root.parent;
+        root.parent = parent.parent;
+        if (parent.parent != null) {
+            if (parent.parent.left == parent) {
+                parent.parent.left = root;
+            }
+            else {
+                parent.parent.right = root;
+            }
+        }
+        RBNode right = root.right;
+        root.right = parent;
+        parent.parent = root;
+        parent.left = right;
+        if (right != null) {
+            right.parent = parent;
+        }
+        if (changeColor) {
+            root.color = Color.BLACK;
+            parent.color = Color.RED;
+        }
+    }
+
+    private void leftRotate(RBNode root, boolean changeColor) {
+        RBNode parent = root.parent;
+        root.parent = parent.parent;
+        if (parent.parent != null) {
+            if (parent.parent.right == parent) {
+                parent.parent.right = root;
+            }
+            else {
+                parent.parent.left = root;
+            }
+        }
+        RBNode left = root.left;
+        root.left = parent;
+        parent.parent = root;
+        parent.right = left;
+        if (left != null) {
+            left.parent = parent;
+        }
+        if (changeColor) {
+            root.color = Color.BLACK;
+            parent.color = Color.RED;
+        }
     }
 
 }

19a3a5ca2163532f596f6716143470332ac0fc95
Yuan Gao <gaoyuanbj@yahoo.com>
2018-04-15 11:49:52 -0400

    Change the test

--- Push-Relabel/src/PushRelabelTest.java
+++ Push-Relabel/src/PushRelabelTest.java
@@ -1,16 +1,14 @@
 public class PushRelabelTest {
     public static void main(String[] args) {
         PushRelabel pr = new PushRelabel(6);
-        pr.addEdge(0, 1, 16);
-        pr.addEdge(0, 2, 13);
-        pr.addEdge(1, 2, 10);
-        pr.addEdge(2, 1, 4);
+        pr.addEdge(0, 1, 11);
+        pr.addEdge(0, 2, 12);
+        pr.addEdge(2, 1, 1);
         pr.addEdge(1, 3, 12);
-        pr.addEdge(2, 4, 14);
-        pr.addEdge(3, 2, 9);
-        pr.addEdge(3, 5, 20);
+        pr.addEdge(2, 4, 11);
+        pr.addEdge(3, 5, 19);
         pr.addEdge(4, 3, 7);
         pr.addEdge(4, 5, 4);
-        System.out.println(pr.getMaxFlow());
+        System.out.println(pr.getMaxFlow());//23
     }
 }
